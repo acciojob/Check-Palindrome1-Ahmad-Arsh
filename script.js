27000800e7d1e8707386521b1d8a17ad08255b71
Arsh Ahmad <123231460+Ahmad-Arsh@users.noreply.github.com>
2023-04-19 00:05:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // complete the given function
 
 function palindrome(str){
+	 s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let left = 0;
+    let right = s.length - 1;
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
 
 }
 module.exports = palindrome
